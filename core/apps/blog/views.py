27c8dad83ead85aa29ea562e74a3589203455b1a
Zaks Danil <zaksdanil1913@gmail.com>
2024-07-20 21:34:57 +0300

    git init

---
+++ core/apps/blog/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+    # return HttpResponse("BLOG")
+
